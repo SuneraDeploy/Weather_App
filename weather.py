f5475ebf1a2512308f76024053e305e006d8fa0f
Sunera Chandeepa Sandaruwan <chandeepasunera@gmail.com>
2025-02-26 11:24:49 +0530

    2.0.4 - Search box coding part updated

--- weather.py
+++ weather.py
@@ -12,4 +12,24 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+#search box
+search_image=PhotoImage(file="search.png")
+myimage=Label(image=search_image)
+myimage.place(x=20,y=20)
+
+textfield=tk.Entry(root,justify="center",width=17,font=("poppins",25,"bold"),bg="#404040",border=0,fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
+
+Search_icon=PhotoImage(file="search_icon.png")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2")
+myimage_icon.place(x=400,y=34)
+
+
+
+
+
+
+
+
 root.mainloop()
