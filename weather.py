debc523e09680a012e0e029c3a3d8778492c750f
Sunera Chandeepa Sandaruwan <chandeepasunera@gmail.com>
2025-02-26 17:27:20 +0530

    2.0.6 - Def weather update

--- weather.py
+++ weather.py
@@ -12,6 +12,20 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False,False)
 
+def getWeather():
+    city=textfield.get()
+
+    geolocator=Nominatim(user_agent="weather_app_1234")
+    location= geolocator.geocode(city)
+    obj =TimezoneFinder()
+    result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
+    print(result)
+
+
+
+
+    
+
 #search box
 search_image=PhotoImage(file="search.png")
 myimage=Label(image=search_image)
@@ -22,7 +36,7 @@ textfield.place(x=50,y=40)
 textfield.focus()
 
 Search_icon=PhotoImage(file="search_icon.png")
-myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040")
+myimage_icon=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
 myimage_icon.place(x=400,y=34)
 
 #logo
@@ -34,6 +48,36 @@ logo.place(x=150,y=100)
 Frame_image=PhotoImage(file="box.png")
 frame_myimage=Label(image=Frame_image)
 frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+#label
+label1=Label(root,text="WIND",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label1.place(x=120,y=400)
+
+label2=Label(root,text="HUMIDITY",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label2.place(x=250,y=400)
+
+label3=Label(root,text="DESCRIPTION",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label3.place(x=430,y=400)
+
+label4=Label(root,text="PRESSURE",font=("Helvetica",15,'bold'),fg="white",bg="#1ab5ef")
+label4.place(x=650,y=400)
+
+t=Label(font=("arial",70,"bold"),fg="#ee666d")
+t.place(x=400,y=150)
+c=Label(font=("arial",15,'bold'))
+c.place(x=400,y=250)
+
+w=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+w.place(x=120,y=430)
+h=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+h.place(x=280,y=430)
+d=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+d.place(x=450,y=430)
+p=Label(text="...",font=("arial",20,"bold"),bg="#1ab5ef")
+p.place(x=670,y=430)
+
+
+
 
 
 
